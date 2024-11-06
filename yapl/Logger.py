38e8d14e09f1f4cae4b658d7211fb175936a4799
Serge46 <serge46b@gmail.com>
2024-11-06 11:44:07 +0700

    added optional datagens and ability to manipulate them

--- yapl/Logger.py
+++ yapl/Logger.py
@@ -1,25 +1,26 @@
 from yapl.errors import LinkToSameDestinationError
-from yapl.styling.StylingABC import LogStyle
-from yapl.styling import StdStyling
+from yapl.styling.stylingABC import LogStyle
+from yapl.styling import standart_styles
+from yapl.datagens import get_caller_location, get_date_and_time
 from typing import Callable, Literal, Any
-from datetime import datetime
-import inspect
 
 
 class BaseLogger:
     __lgr_dst: Literal["stdout"] | str | None
     __logging_callback: Callable[[dict], None]
+    __optional_data_gens: dict[str, Callable[[], Any]]
     style: LogStyle
 
     def __init__(
         self,
         logging_callback: Callable[[dict], None],
         log_destination: Literal["stdout"] | str | None = None,
-        style: LogStyle = StdStyling.stdout,
+        style: LogStyle = standart_styles.stdout,
     ):
         self.__lgr_dst = log_destination
         self.__logging_callback = logging_callback
         self.style = style
+        self.__optional_data_gens = {}
         if log_destination is not None and log_destination != "stdout":
             f = open(log_destination, "w")
             f.close()
@@ -36,9 +37,9 @@ class BaseLogger:
 
         return lg
 
-    def __get_caller_location(self) -> str:
-        caller_frame = inspect.stack()[3]
-        return caller_frame.filename
+    @property
+    def optional_datagens(self) -> dict[str, Callable[[], Any]]:
+        return self.__optional_data_gens
 
     def __log_stdout(self, string: str) -> None:
         print(string)
@@ -55,16 +56,14 @@ class BaseLogger:
 
     def log(self, evt_type: str, msg: str) -> None:
         log_obj = {"event_type": evt_type, "message": msg}
-        log_obj["location"] = self.__get_caller_location()
-        date_and_time = {}
-        date_and_time["year"] = datetime.now().year
-        date_and_time["month"] = datetime.now().month
-        date_and_time["day"] = datetime.now().day
-        date_and_time["hour"] = datetime.now().hour
-        date_and_time["minute"] = datetime.now().minute
-        date_and_time["second"] = datetime.now().second
-        date_and_time["microsecond"] = datetime.now().microsecond
-        log_obj.update(date_and_time)
+        for key in self.__optional_data_gens:
+            val = self.__optional_data_gens[key]()
+            if type(val) == dict:
+                log_obj.update(val)
+                continue
+            log_obj[key] = val
+        # log_obj["location"] = self.__get_caller_location()
+        # log_obj.update(date_and_time)
         self.__logging_callback(log_obj)
         self.log_json(log_obj)
 
@@ -76,13 +75,16 @@ class BaseLogger:
 class LoggerContainer:
     __passive_loggers: list[BaseLogger]
     __active_loggers: list[BaseLogger]
+    __optional_data_gens: dict[str, Callable[[], Any]]
 
     def __init__(self):
         self.__passive_loggers = []
         self.__active_loggers = []
+        self.__optional_data_gens = {}
 
     def __call__(self):
         self.__passive_loggers.append(BaseLogger(self.__gen_logger_callback()))
+        self.__passive_loggers[-1].optional_datagens.update(self.__optional_data_gens)
         return self.__passive_loggers[-1]
 
     def __gen_logger_callback(self):
@@ -92,7 +94,7 @@ class LoggerContainer:
 
         return lgr_cb
 
-    def add_logger(
+    def add_log_destination(
         self,
         destination: Literal["stdout"] | str | Callable[[dict], None],
         style: LogStyle,
@@ -116,6 +118,13 @@ class LoggerContainer:
             if lgr.destination == dst:
                 lgr.style = new_style
 
+    def update_optional_datagens(self, key: str, datagen: Callable[[], Any]) -> None:
+        self.__optional_data_gens[key] = datagen
+        for lgr in self.__passive_loggers:
+            lgr.optional_datagens[key] = datagen
+
 
 Logger = LoggerContainer()
-Logger.add_logger("stdout", StdStyling.stdout)
+Logger.add_log_destination("stdout", standart_styles.stdout)
+Logger.update_optional_datagens("location", get_caller_location)
+Logger.update_optional_datagens("date and time", get_date_and_time)
---
+++ yapl/datagens.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import inspect
+
+
+def get_caller_location() -> str:
+    caller_frame = inspect.stack()[3]
+    return caller_frame.filename.replace("\\", "/")
+
+
+def get_date_and_time() -> dict:
+    date_and_time = {}
+    date_and_time["year"] = datetime.now().year
+    date_and_time["month"] = datetime.now().month
+    date_and_time["day"] = datetime.now().day
+    date_and_time["hour"] = datetime.now().hour
+    date_and_time["minute"] = datetime.now().minute
+    date_and_time["second"] = datetime.now().second
+    date_and_time["microsecond"] = datetime.now().microsecond
+    return date_and_time
