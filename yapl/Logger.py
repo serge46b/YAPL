35f23bcf45bafb547769911a8ce27adff4644274
Serge46 <serge46b@gmail.com>
2024-11-11 14:31:48 +0700

    Implemented strings with fixed width, partially implemented sticky strings

--- yapl/Logger.py
+++ yapl/Logger.py
@@ -2,9 +2,11 @@ from yapl.errors import LinkToSameDestinationError
 from yapl.styling.stylingABC import LogStyle
 from yapl.styling import basic_styles
 from yapl.basic_datagens import get_caller_location, get_date_and_time
+from yapl.sticky.strs import StickyString
 from typing import Callable, Literal, Any, Optional
 from abc import ABC, abstractmethod
 from io import TextIOWrapper
+import os
 
 
 class BaseLogger(ABC):
@@ -84,6 +86,7 @@ class BaseLogger(ABC):
 
 class ConsoleLogger(BaseLogger):
     __log_destination: Literal["stdout"]
+    __sticky_strings: list[StickyString]
 
     def __init__(
         self,
@@ -101,9 +104,29 @@ class ConsoleLogger(BaseLogger):
             ininial_callback,
             destruction_callback,
         )
+        self.__sticky_strings = []
 
-    def log_raw(self, raw_msg):
-        print(raw_msg)
+    def add_sticky_string(self, s_str: StickyString) -> None:
+        self.__sticky_strings.append(s_str)
+
+    def rem_sticky_strings(self, s_str: StickyString) -> None:
+        try:
+            self.__sticky_strings.remove(s_str)
+        except ValueError:
+            return
+
+    def log_raw(self, raw_msg) -> None:
+        terminal_width = os.get_terminal_size().columns
+        padding_size = terminal_width - (len(raw_msg) % terminal_width)
+        self.__internal_log_raw(raw_msg + " " * padding_size + "\n")
+        self.__log_sticky()
+
+    def __log_sticky(self) -> None:
+        for s_str in self.__sticky_strings:
+            print(s_str.summary_string)
+
+    def __internal_log_raw(self, raw_msg) -> None:
+        print(raw_msg, end="")
 
 
 class FileLogger(BaseLogger):
@@ -175,91 +198,6 @@ class FunctionLogger(BaseLogger):
         self.__log_destination(raw_msg)
 
 
-# class BaseLogger:
-#     __lgr_dst: Literal["stdout"] | str | None
-#     __logging_callback: Callable[[dict], None]
-#     __optional_data_gens: dict[str, Callable[[], Any]]
-#     style: LogStyle
-#     init_callback: Optional[Callable]
-#     del_callback: Optional[Callable]
-
-#     def __init__(
-#         self,
-#         logging_callback: Callable[[dict], None],
-#         log_destination: Literal["stdout"] | str | None = None,
-#         style: LogStyle = basic_styles.stdout,
-#         init_callback: Optional[Callable] = None,
-#         finish_callback: Optional[Callable] = None,
-#     ):
-#         self.__lgr_dst = log_destination
-#         self.__logging_callback = logging_callback
-#         self.style = style
-#         self.__optional_data_gens = {}
-#         if log_destination is not None and log_destination != "stdout":
-#             self.__f = open(log_destination, "w")
-#         self.init_callback = init_callback
-#         self.finish_callback = finish_callback
-#         if init_callback is not None:
-#             init_callback(self)
-
-#     def __del__(self):
-#         if self.finish_callback is not None:
-#             self.finish_callback(self)
-#         if self.__lgr_dst is not None and self.__lgr_dst != "stdout":
-#             self.__f.close()
-
-#     def __getattr__(self, name: str) -> Any:
-#         norm_name = name.upper()
-#         if not self.style.is_event(norm_name):
-#             raise AttributeError(
-#                 f"'{BaseLogger.__name__}' object has no attribute '{name}'"
-#             )
-
-#         def lg(msg: str):
-#             self.log(norm_name, msg)
-
-#         return lg
-
-#     def __log_stdout(self, string: str) -> None:
-#         print(string)
-
-#     def log_file(self, string: str) -> None:
-#         self.__f.write(string)
-
-#     @property
-#     def optional_datagens(self) -> dict[str, Callable[[], Any]]:
-#         return self.__optional_data_gens
-
-#     @property
-#     def destination(self):
-#         return self.__lgr_dst
-
-#     def log_json(self, json: dict) -> None:
-#         if self.__lgr_dst is None:
-#             self.__logging_callback(json)
-#             return
-#         if self.__lgr_dst == "stdout":
-#             self.__log_stdout(self.style.to_format_string(json))
-#             return
-#         self.__f.write(self.style.to_format_string(json))
-#         self.__f.write("\n")
-
-#     def log(self, evt_type: str, msg: str) -> None:
-#         log_obj = {"event_type": evt_type, "message": msg}
-#         for key in self.__optional_data_gens:
-#             val = self.__optional_data_gens[key]()
-#             if type(val) == dict:
-#                 log_obj.update(val)
-#                 continue
-#             log_obj[key] = val
-#         # log_obj["location"] = self.__get_caller_location()
-#         # log_obj.update(date_and_time)
-#         self.__logging_callback(log_obj)
-#         if self.__lgr_dst is None:
-#             return
-#         self.log_json(log_obj)
-
-
 class LoggerContainer:
     __passive_loggers: list[BaseLogger]
     __active_loggers: dict[str | Callable, BaseLogger]
@@ -284,6 +222,7 @@ class LoggerContainer:
             kwargs["logging_callback"] = new_cb
         else:
             kwargs["logging_callback"] = std_cb
+
         kwargs.pop("style", None)
         self.__passive_loggers.append(FunctionLogger(self.__event_types_list, **kwargs))
         self.__passive_loggers[-1].optional_datagens.update(self.__optional_data_gens)
