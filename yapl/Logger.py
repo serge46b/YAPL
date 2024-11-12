468c279d680ad6c178bedc183dfc2519b80dc493
Serge46 <serge46b@gmail.com>
2024-11-12 17:32:18 +0700

    Conatiner logic modified.

--- yapl/Logger.py
+++ yapl/Logger.py
@@ -175,96 +175,45 @@ class FunctionLogger(BaseLogger):
         self.__log_destination(raw_msg)
 
 
-# class BaseLogger:
-#     __lgr_dst: Literal["stdout"] | str | None
-#     __logging_callback: Callable[[dict], None]
-#     __optional_data_gens: dict[str, Callable[[], Any]]
-#     style: LogStyle
-#     init_callback: Optional[Callable]
-#     del_callback: Optional[Callable]
+class ContaineredLogger:
+    __active_loggers: dict[str | Callable, ConsoleLogger | FileLogger | FunctionLogger]
+    event_types: list[str] | Literal["any"]
 
-#     def __init__(
-#         self,
-#         logging_callback: Callable[[dict], None],
-#         log_destination: Literal["stdout"] | str | None = None,
-#         style: LogStyle = basic_styles.stdout,
-#         init_callback: Optional[Callable] = None,
-#         finish_callback: Optional[Callable] = None,
-#     ):
-#         self.__lgr_dst = log_destination
-#         self.__logging_callback = logging_callback
-#         self.style = style
-#         self.__optional_data_gens = {}
-#         if log_destination is not None and log_destination != "stdout":
-#             self.__f = open(log_destination, "w")
-#         self.init_callback = init_callback
-#         self.finish_callback = finish_callback
-#         if init_callback is not None:
-#             init_callback(self)
+    def __init__(
+        self,
+        active_loggers: dict[
+            str | Callable, ConsoleLogger | FileLogger | FunctionLogger
+        ],
+        event_types: list[str] | Literal["any"],
+    ) -> None:
+        self.__active_loggers = active_loggers
+        self.event_types = event_types
 
-#     def __del__(self):
-#         if self.finish_callback is not None:
-#             self.finish_callback(self)
-#         if self.__lgr_dst is not None and self.__lgr_dst != "stdout":
-#             self.__f.close()
+    def __getitem__(self, key: str) -> ConsoleLogger | FileLogger | FunctionLogger:
+        return self.__active_loggers[key]
 
-#     def __getattr__(self, name: str) -> Any:
-#         norm_name = name.upper()
-#         if not self.style.is_event(norm_name):
-#             raise AttributeError(
-#                 f"'{BaseLogger.__name__}' object has no attribute '{name}'"
-#             )
+    def __getattr__(self, name: str) -> Any:
+        norm_name = name.upper()
+        if self.event_types != "any" and norm_name not in self.event_types:
+            raise AttributeError(
+                f"'{ContaineredLogger.__name__}' object has no attribute '{name}'"
+            )
 
-#         def lg(msg: str):
-#             self.log(norm_name, msg)
+        def lg(msg: str):
+            self.log_message(norm_name, msg)
 
-#         return lg
+        return lg
 
-#     def __log_stdout(self, string: str) -> None:
-#         print(string)
-
-#     def log_file(self, string: str) -> None:
-#         self.__f.write(string)
-
-#     @property
-#     def optional_datagens(self) -> dict[str, Callable[[], Any]]:
-#         return self.__optional_data_gens
-
-#     @property
-#     def destination(self):
-#         return self.__lgr_dst
-
-#     def log_json(self, json: dict) -> None:
-#         if self.__lgr_dst is None:
-#             self.__logging_callback(json)
-#             return
-#         if self.__lgr_dst == "stdout":
-#             self.__log_stdout(self.style.to_format_string(json))
-#             return
-#         self.__f.write(self.style.to_format_string(json))
-#         self.__f.write("\n")
-
-#     def log(self, evt_type: str, msg: str) -> None:
-#         log_obj = {"event_type": evt_type, "message": msg}
-#         for key in self.__optional_data_gens:
-#             val = self.__optional_data_gens[key]()
-#             if type(val) == dict:
-#                 log_obj.update(val)
-#                 continue
-#             log_obj[key] = val
-#         # log_obj["location"] = self.__get_caller_location()
-#         # log_obj.update(date_and_time)
-#         self.__logging_callback(log_obj)
-#         if self.__lgr_dst is None:
-#             return
-#         self.log_json(log_obj)
+    def log_message(self, evt_type: str, msg: str) -> None:
+        for lgr in self.__active_loggers.values():
+            lgr.log_message(evt_type, msg)
 
 
 class LoggerContainer:
-    __passive_loggers: list[BaseLogger]
-    __active_loggers: dict[str | Callable, BaseLogger]
-    __optional_data_gens: dict[str, Callable[[], Any]]
+    __passive_loggers: list[ContaineredLogger]
+    __active_loggers: dict[str | Callable, ConsoleLogger | FileLogger | FunctionLogger]
     __event_types_list: list[str] | Literal["any"]
+    __optional_data_gens: dict[str, Callable[[], Any]]
 
     def __init__(self):
         self.__passive_loggers = []
@@ -272,29 +221,14 @@ class LoggerContainer:
         self.__optional_data_gens = {}
         self.__event_types_list = basic_styles.STANDART_EVENT_TYPES
 
-    def __call__(self, **kwargs):
-        std_cb = self.__gen_logger_callback()
-        if "logging_callback" in kwargs:
-            old_cb = kwargs["logging_callback"]
-
-            def new_cb(json):
-                std_cb(json)
-                old_cb(json)
-
-            kwargs["logging_callback"] = new_cb
-        else:
-            kwargs["logging_callback"] = std_cb
-        kwargs.pop("style", None)
-        self.__passive_loggers.append(FunctionLogger(self.__event_types_list, **kwargs))
-        self.__passive_loggers[-1].optional_datagens.update(self.__optional_data_gens)
+    def __call__(self):
+        self.__passive_loggers.append(
+            ContaineredLogger(self.__active_loggers, self.__event_types_list)
+        )
         return self.__passive_loggers[-1]
 
-    def __gen_logger_callback(self):
-        def lgr_cb(json: dict) -> None:
-            for lgr in self.__active_loggers:
-                self.__active_loggers[lgr].log_json(json)
-
-        return lgr_cb
+    def __getitem__(self, key: str) -> ConsoleLogger | FileLogger | FunctionLogger:
+        return self.__active_loggers[key]
 
     def add_log_destination(
         self,
@@ -314,6 +248,9 @@ class LoggerContainer:
             self.__active_loggers[log_destination] = ConsoleLogger(
                 style, self.__event_types_list, **kwargs
             )
+            self.__active_loggers[log_destination].optional_datagens.update(
+                self.__optional_data_gens
+            )
             return
         if isinstance(log_destination, str):
             if style is None:
@@ -323,9 +260,15 @@ class LoggerContainer:
             self.__active_loggers[log_destination] = FileLogger(
                 log_destination, style, self.__event_types_list, **kwargs
             )
+            self.__active_loggers[log_destination].optional_datagens.update(
+                self.__optional_data_gens
+            )
             return
         self.__active_loggers[log_destination] = FunctionLogger(
             self.__event_types_list, style, log_destination, **kwargs
+        )
+        self.__active_loggers[log_destination].optional_datagens.update(
+            self.__optional_data_gens
         )
 
     def update_style(
@@ -340,10 +283,10 @@ class LoggerContainer:
         for lgr in self.__active_loggers.values():
             lgr.event_types = self.__event_types_list
 
-    def update_optional_datagens(self, key: str, datagen: Callable[[], Any]) -> None:
-        self.__optional_data_gens[key] = datagen
-        for lgr in self.__passive_loggers:
-            lgr.optional_datagens[key] = datagen
+    def update_optional_datagens(self, name: str, datagen: Callable[[], Any]) -> None:
+        self.__optional_data_gens[name] = datagen
+        for lgr in self.__active_loggers.values():
+            lgr.optional_datagens[name] = datagen
 
 
 Logger = LoggerContainer()
