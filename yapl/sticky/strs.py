4ca75dd8aa95b603960886b2e92c3eae3611d973
Serge46 <serge46b@gmail.com>
2024-12-19 15:40:58 +0700

    Added some optimization to WidthNormilizedString

--- yapl/sticky/strs.py
+++ yapl/sticky/strs.py
@@ -38,9 +38,11 @@ def visible_cut_from_to(s: str, st_idx: int = 0, end_idx: Optional[int] = None):
 
 class WidthNormilizedString:
     __string: str
-    width: int
-    align: Literal["left", "center", "right"]
-    string_shift: int = 0
+    __width: int
+    __align: Literal["left", "center", "right"]
+    __string_shift: int = 0
+
+    __cached_str: Optional[str] = None
 
     def __init__(
         self,
@@ -51,59 +53,106 @@ class WidthNormilizedString:
         self.__string = string
         if width is None:
             width = visible_length(self.__string)
-        self.width = width
-        self.align = align
+        self.__width = width
+        self.__align = align
 
     def update_str(self, string: str, update_width: bool = True):
+        self.__cached_str = None
         self.__string = string
         if update_width:
-            self.width = visible_length(self.__string)
+            self.__width = visible_length(self.__string)
 
     def shift_str(self) -> None:
-        self.string_shift += 1
-        if self.string_shift >= self.width + visible_length(self.__string):
-            self.string_shift = 0
+        self.__cached_str = None
+        self.__string_shift += 1
+        if self.__string_shift >= self.__width + visible_length(self.__string):
+            self.__string_shift = 0
 
     @property
     def string(self) -> str:
+        if self.__cached_str is not None:
+            return self.__cached_str
         str_len = visible_length(self.__string)
-        if str_len > self.width:
-            end_idx = self.string_shift + self.width
-            if end_idx > str_len + self.width:
-                padding = " " * (str_len + self.width * 2 - end_idx)
-                # return padding + self.__string[: (end_idx - str_len - self.width)]
-                return padding + visible_cut_from_to(
-                    self.__string, 0, end_idx - str_len - self.width
+        if str_len > self.__width:
+            end_idx = self.__string_shift + self.__width
+            if end_idx > str_len + self.__width:
+                padding = " " * (str_len + self.__width * 2 - end_idx)
+                ret_str = padding + visible_cut_from_to(
+                    self.__string, 0, end_idx - str_len - self.__width
                 )
+                self.__cached_str = ret_str
+                return ret_str
             if end_idx > str_len:
                 padding = " " * (end_idx - str_len)
-                # return self.__string[self.string_shift :] + padding
-                return visible_cut_from_to(self.__string, self.string_shift) + padding
-            # return self.__string[self.string_shift : end_idx]
-            return visible_cut_from_to(self.__string, self.string_shift, end_idx)
+                ret_str = (
+                    visible_cut_from_to(self.__string, self.__string_shift) + padding
+                )
+                self.__cached_str = ret_str
+                return ret_str
+            ret_str = visible_cut_from_to(self.__string, self.__string_shift, end_idx)
+            self.__cached_str = ret_str
+            return ret_str
 
-        if self.align == "center":
-            padding = " " * ((self.width - str_len) // 2)
-            return padding + self.__string + padding
-        padding = " " * (self.width - str_len)
-        if self.align == "left":
-            return self.__string + padding
-        return padding + self.__string
+        if self.__align == "center":
+            padding = " " * ((self.__width - str_len) // 2)
+            ret_str = padding + self.__string + padding
+            self.__cached_str = ret_str
+            return ret_str
+        padding = " " * (self.__width - str_len)
+        if self.__align == "left":
+            ret_str = self.__string + padding
+            self.__cached_str = ret_str
+            return ret_str
+        ret_str = padding + self.__string
+        self.__cached_str = ret_str
+        return ret_str
 
     @string.setter
     def string(self, new_str: str) -> None:
+        self.__cached_str = None
         self.__string = new_str
-        self.string_shift = 0
+        self.__string_shift = 0
 
     @property
     def raw_str(self) -> str:
         return self.__string
+
+    @property
+    def width(self) -> int:
+        return self.__width
+
+    @width.setter
+    def width(self, new_width: int) -> None:
+        self.__cached_str = None
+        self.__width = new_width
+
+    @property
+    def align(self) -> Literal["left", "center", "right"]:
+        return self.__align
+
+    @align.setter
+    def align(self, new_align: Literal["left", "center", "right"]) -> None:
+        self.__cached_str = None
+        self.__align = new_align
+
+    @property
+    def string_shift(self) -> int:
+        return self.__string_shift
+
+    @string_shift.setter
+    def string_shift(self, new_string_shift: int) -> None:
+        self.__cached_str = None
+        self.__string_shift = new_string_shift
+        max_shift = self.__width + visible_length(self.__string)
+        if self.__string_shift >= max_shift:
+            self.__string_shift -= max_shift
 
 
 class StickyString:
     __wn_strs: list[WidthNormilizedString]
     __wn_strs_width: list[float | int | Literal["auto"]]
 
+    # TODO: remake constructor arguments (because separate string list and parameters list is inconvinient.)
     def __init__(self, strs: list[str], params: Optional[list[dict]] = None):
         self.__wn_strs = []
         self.__wn_strs_width = []
@@ -130,12 +179,12 @@ class StickyString:
         for i, wn_str in enumerate(self.__wn_strs):
             wd = self.__wn_strs_width[i]
             if type(wd) == int:
-                wn_str.width = wd
+                wn_str.__width = wd
                 width_left -= wd
                 continue
             if type(wd) == float:
                 wd = int(wd * term_width)
-                wn_str.width = wd
+                wn_str.__width = wd
                 width_left -= wd
                 continue
             auto_sized_strs.append(wn_str)
